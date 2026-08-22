08d274533fef3ccd97ecafc389ac36cc84fa468a
ARENAM10 <amiralimazraei1385812e@gmail.com>
2026-08-22 18:20:42 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-const token =8850301156:AAFHdCOBvS5hOW-QnhndZm6wXr9W8v5lNMw
-
-if (!token) {
-  console.error("❌ Error: BOT_TOKEN is not set!");
-  process.exit(1);
-}
+const token = 8850301156:AAFHdCOBvS5hOW-QnhndZm6wXr9W8v5lNMw
 
 const bot = new TelegramBot(token, { polling: true });
 
-console.log("🤖 Bot is running successfully...");
+console.log("Bot is running successfully...");
 
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "سلام! ربات شما با موفقیت از Replit به Railway منتقل شد و روشن است. 🚀");
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "سلام! ربات شما با موفقیت روشن شد.");
 });
+
